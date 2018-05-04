7a9fc788c121b4d515917e68f1af01193dc9aeb6
yikaiw <845002185@qq.com>
2018-05-04 15:52:41 +0800

    main

--- main.py
+++ main.py
@@ -32,50 +32,55 @@ def main():
     saver = tf.train.Saver()
 
     print('Build session.')
-    sess = tf.Session()
-    if FLAGS.pretrained is not None:
-        checkpoint = tf.train.get_checkpoint_state(checkpoint_path)
-        meta_graph_path = checkpoint.model_checkpoint_path + '.meta'
-        restore = tf.train.import_meta_graph(meta_graph_path)
-        restore.restore(sess, tf.train.latest_checkpoint(checkpoint_path))
-        step = int(meta_graph_path.split('-')[2].split('.')[0])
-    else:
-        sess.run(tf.global_variables_initializer())
-        step = 0
+    with tf.Session() as sess:
+        if FLAGS.pretrained is not None:
+            checkpoint = tf.train.get_checkpoint_state(checkpoint_path)
+            meta_graph_path = checkpoint.model_checkpoint_path + '.meta'
+            restore = tf.train.import_meta_graph(meta_graph_path)
+            restore.restore(sess, tf.train.latest_checkpoint(checkpoint_path))
+            step = int(meta_graph_path.split('-')[2].split('.')[0])
+        else:
+            sess.run(tf.global_variables_initializer())
+            step = 0
 
-    # train_writer = tf.summary.FileWriter('log', sess.graph)
-    # summary_op = tf.summary.merge_all()
+        # train_writer = tf.summary.FileWriter('log', sess.graph)
+        # summary_op = tf.summary.merge_all()
 
-    coord = tf.train.Coordinator()
-    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+        try:
+            print('Start training:')
+            while not coord.should_stop():
+                X_train_batch, y_train_batch = sess.run([read_for_train.X_batch, read_for_train.y_batch])
+                loss, _ = sess.run([cnn.loss, cnn.optimizer],
+                    {cnn.X_inputs: X_train_batch, cnn.y_inputs: y_train_batch, cnn.training: True})
 
-    print('Start training:')
-    while not coord.should_stop():
-        X_train_batch, y_train_batch = sess.run([read_for_train.X_batch, read_for_train.y_batch])
-        loss, _ = sess.run([cnn.loss, cnn.optimizer],
-            {cnn.X_inputs: X_train_batch, cnn.y_inputs: y_train_batch, cnn.training: True})
+                X_val_batch, y_val_batch = sess.run([read_for_val.X_batch, read_for_val.y_batch])
+                accuracy = sess.run(cnn.accuracy,
+                    {cnn.X_inputs: X_val_batch, cnn.y_inputs: y_val_batch, cnn.training: False})
 
-        X_val_batch, y_val_batch = sess.run([read_for_val.X_batch, read_for_val.y_batch])
-        accuracy = sess.run(cnn.accuracy,
-            {cnn.X_inputs: X_val_batch, cnn.y_inputs: y_val_batch, cnn.training: False})
+                # train_writer.add_summary(summary, step)
+                # train_writer.flush()
 
-        # train_writer.add_summary(summary, step)
-        # train_writer.flush()
+                if step % 100 == 0:
+                    print('At step {}:'.format(step))
+                    print('\tloss: {}'.format(loss))
+                    print('\taccuracy: {}'.format(accuracy))
+                if step % 10000 == 0 and step > 0:
+                    save_path = saver.save(sess, checkpoint_path + '/model.ckpt', global_step=step)
+                    print('Model saved in file: %s' % save_path)
+                step += 1
 
-        if step % 100 == 0:
-            print('At step {}:'.format(step))
-            print('\tloss: {}'.format(loss))
-            print('\taccuracy: {}'.format(accuracy))
-        if step % 10000 == 0 and step > 0:
+        except KeyboardInterrupt:
+            print('Interrupted')
+            coord.request_stop()
+        except Exception as e:
+            coord.request_stop(e)
+        finally:
             save_path = saver.save(sess, checkpoint_path + '/model.ckpt', global_step=step)
             print('Model saved in file: %s' % save_path)
-        step += 1
-
-    save_path = saver.save(sess, checkpoint_path + '/model.ckpt', global_step=step)
-    print('Model saved in file: %s' % save_path)
-    coord.request_stop()
-    coord.join(threads)
-    sess.close()
+            coord.request_stop()
+            coord.join(threads)
 
 
 if __name__ == '__main__':
